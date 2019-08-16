7bcf2942a0f0217feec21aac1604dde55e5e75d4
Nick Miner <Nick.Miner18@gmail.com>
2019-08-16 14:14:36 -0600

    finished joins and join challenges

--- SQL-Practice/customers_orders.sql
+++ SQL-Practice/customers_orders.sql
@@ -50,3 +50,4 @@ SELECT first_name, last_name, order_date, SUM(amount) FROM customers LEFT JOIN o
 
 SELECT first_name, last_name, order_date, IFNULL(SUM(amount), 0) AS total_spent FROM customers LEFT JOIN orders ON customers.id = orders.customer_id GROUP BY customers.id ORDER BY total_spent;
 
+SELECT * from customers RIGHT JOIN orders ON customers.id = orders.customer_id;
